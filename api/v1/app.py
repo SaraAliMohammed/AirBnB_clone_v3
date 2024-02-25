cef8ae8e963e876524d139dd74800dba5ee3f85a
SaraAliMohammed <sara.ali.mohammed213@gmail.com>
2024-02-25 04:59:52 -0800

    Edit Task5

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ Script that imports a Blueprint and runs Flask """
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -14,6 +14,11 @@ app.register_blueprint(app_views)
 def teardown_session(exception):
     """ Closes storage session """
     storage.close()
+
+@app.errorhandler(404)
+def page_not_foun(error):
+    """ Loads a custom 404 page not found """
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
 if __name__ == "__main__":
