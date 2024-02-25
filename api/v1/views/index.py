51cfe47de989687c8176ad178d9b8935d01c079d
SaraAliMohammed <sara.ali.mohammed213@gmail.com>
2024-02-25 04:46:34 -0800

    Edit Task3

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 """ Index """
-from api.v1.views import app_views
 from flask import jsonify
 from models import storage
+from api.v1.views import app_views
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """ Returns JSON """
     return jsonify(status="OK")
+
 
 @app_views.route('/stats', methods=['GET'], strict_slashes=False)
 def stats():
